3adf9a8c86c04a0f6a26508bec0ec26e92874246
artiecannv <thefiftharthur@gmail.com>
2022-12-05 13:57:53 -0700

    fixed bootstrap link

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const APIkey = "945b04e321728f7351f85b9767317c51";
+let city;
+let state;
+let country;
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={APIkey}
+
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey};
--- script.js
+++
@@ -1,6 +0,0 @@
-const APIkey = "945b04e321728f7351f85b9767317c51";
-let city;
-let state;
-let country;
-
-
