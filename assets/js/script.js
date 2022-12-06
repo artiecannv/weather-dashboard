92c42e52c7592b159ea36411269c75441e5e97e5
artiecannv <thefiftharthur@gmail.com>
2022-12-05 18:02:00 -0700

    attempting api stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 const APIkey = "945b04e321728f7351f85b9767317c51";
-let city;
-let state;
-let country;
 
-// http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={APIkey}
+let zipCode;
 
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey};
+// api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={API key}
