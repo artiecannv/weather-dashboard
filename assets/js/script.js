2f60b5d52672c5205e7cb0241d88a38cd1d23593
artiecannv <thefiftharthur@gmail.com>
2022-12-11 08:54:26 -0700

    started filling the answers out on my template block

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,39 @@ searchButton.addEventListener("click", getWeather);
 
 function getWeather(event, countryCode = 'US') {
     event.preventDefault();
+     // Getting the current weather
+     fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${inputBox.value},${countryCode}&appid=${APIkey}`).then(response => {return response.json()}).then(weatherObject => {
+        console.log(weatherObject);
+
+
+
+    
+    }) 
+
     // Getting the future weather
     fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${inputBox.value},${countryCode}&appid=${APIkey}`).then(response => {return response.json()}).then(weatherObject => {
         console.log(weatherObject);
+        
+        let dateTime = new Date(weatherObject.list[4].dt * 1000);
+        let date = dateTime.toLocaleDateString("en-US");
+        let time = dateTime.toTimeString("it-IT");
+        let location = weatherObject.city.name;
+        let hiLow = [weatherObject.list[4].main.temp_max, weatherObject.list[4].main.temp_min]
 
-    // let dateTime = new Date(weatherObject.list[i].dt * 1000);
-    // let date = dateTime.toLocaleDateString("en-US");
-    // let time = dateTime.toTimeString("it-IT");
+
+        document.getElementById('date').innerHTML = dateTime;
+        document.getElementById('location').innerHTML = location;
+        document.getElementById('hiLow').innerHTML = hiLow
+
+   
+
+
+
+
+
+
+
 
     }) 
-    // Getting the current weather
-    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${inputBox.value},${countryCode}&appid=${APIkey}`).then(response => {return response.json()}).then(weatherObject => {
-        console.log(weatherObject);
-
-    // let dateTime = new Date(weatherObject.list[i].dt * 1000);
-    // let date = dateTime.toLocaleDateString("en-US");
-    // let time = dateTime.toTimeString("it-IT");
-
-    }) 
+   
 };
