124f49b224d38df2feb5f2879e67b179ed7581ba
artiecannv <thefiftharthur@gmail.com>
2023-01-11 15:04:33 -0700

    made 5 day forecast cards w template text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,66 @@
 const APIkey = "945b04e321728f7351f85b9767317c51";
-
 let searchButton = document.getElementById("srchBtn");
 let inputBox = document.getElementById("inputBox");
 
-
 searchButton.addEventListener("click", getWeather);
 
-function getWeather(event, countryCode = 'US') {
-    event.preventDefault();
-     // Getting the current weather
-     fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${inputBox.value},${countryCode}&appid=${APIkey}`).then(response => {return response.json()}).then(weatherObject => {
-        console.log(weatherObject);
-        let currentDate = new Date(weatherObject.dt * 1000);
-        let currentTemp = ["Current Temperature is:" + " " + (((weatherObject.main.temp - 273) * 1.8) + 32) + "&#8457;"];
-        let currentHumid = ['Humidity is:' + ' ' + (weatherObject.main.humidity) + '%'];
-        // let currentWind = ;
+function getWeather(event, countryCode = "US") {
+  event.preventDefault();
+  // Getting the current weather
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?zip=${inputBox.value},${countryCode}&appid=${APIkey}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((weatherObject) => {
+      console.log(weatherObject);
+      let currentDate = new Date(weatherObject.dt * 1000);
+      let currentTemp = [
+        "Current Temperature is:" +
+          " " +
+          ((weatherObject.main.temp - 273) * 1.8 + 32) +
+          "&#8457;",
+      ];
+      let currentHumid = [
+        "Humidity is:" + " " + weatherObject.main.humidity + "%",
+      ];
+      let currentWind = [weatherObject.wind.speed * 2.237 + "mph"];
 
+      document.getElementById("currentDate").innerHTML = currentDate;
+      document.getElementById("currentTemp").innerHTML = currentTemp;
+      document.getElementById("currentHumid").innerHTML = currentHumid;
+      document.getElementById("currentWind").innerHTML = currentWind;
+      
+    });
 
-        document.getElementById('currentDate').innerHTML = currentDate
-        document.getElementById('currentTemp').innerHTML = currentTemp;
-        document.getElementById('currentHumid').innerHTML = currentHumid;
+  // Getting the future weather
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?zip=${inputBox.value},${countryCode}&appid=${APIkey}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((weatherObject) => {
+      console.log(weatherObject);
 
+      let dateTime = new Date(weatherObject.list[2].dt * 1000);
+      let date = dateTime.toLocaleDateString("en-US");
+      let time = dateTime.toTimeString("it-IT");
+      let location = weatherObject.city.name;
+      let hiLow = [
+        weatherObject.list[2].main.temp_max,
+        weatherObject.list[2].main.temp_min,
+      ];
+      let humidity = [
+        "Humidity is:" + " " + weatherObject.list[4].main.humidity + "%",
+      ];
+      let weatherIcon = [weatherObject.list[4].weather.icon];
 
-
-    
-    }) 
-
-    // Getting the future weather
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${inputBox.value},${countryCode}&appid=${APIkey}`).then(response => {return response.json()}).then(weatherObject => {
-        console.log(weatherObject);
-        
-        let dateTime = new Date(weatherObject.list[2].dt * 1000);
-        let date = dateTime.toLocaleDateString("en-US");
-        let time = dateTime.toTimeString("it-IT");
-        let location = weatherObject.city.name;
-        let hiLow = [weatherObject.list[2].main.temp_max, weatherObject.list[2].main.temp_min];
-        let humidity = ['Humidity is:' + ' ' + (weatherObject.list[4].main.humidity) + '%'];
-        let weatherIcon = [weatherObject.list[4].weather.icon]
-
-
-        document.getElementById('date').innerHTML = dateTime;
-        document.getElementById('location').innerHTML = location;
-        document.getElementById('hiLow').innerHTML = hiLow;
-        document.getElementById('humid').innerHTML = humidity;
-        document.getElementById('weatherIcon').innerHTML = weatherIcon;
-        
-
-   
-
-
-
-
-
-
-
-
-    }) 
-   
-};
+      document.getElementById("date").innerHTML = dateTime;
+      document.getElementById("location").innerHTML = location;
+      document.getElementById("hiLow").innerHTML = hiLow;
+      document.getElementById("humid").innerHTML = humidity;
+      document.getElementById("weatherIcon").innerHTML = weatherIcon;
+    });
+}
