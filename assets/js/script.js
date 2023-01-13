1974c47dfbc584380c1c43ce2edd4484b9e298e4
artiecannv <thefiftharthur@gmail.com>
2023-01-12 19:23:32 -0700

    finished functionality of my website. formatting is the only thing necessary

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,17 @@ function getWeather(event, countryCode = "US") {
     .then((weatherObject) => {
       console.log(weatherObject);
       let currentDate = new Date(weatherObject.dt * 1000);
-      let currentDateEl = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-      let location = weatherObject.name
+      let currentDateEl = `${
+        currentDate.getMonth() + 1
+      }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+      let location = weatherObject.name;
       let currentTemp = [
-        "Current Temperature is:" +
-          " " +
-          weatherObject.main.temp +
-          "&#8457;",
+        "Current Temperature is:" + " " + weatherObject.main.temp + "&#8457;",
       ];
-      let currentHumid = "Humidity is:" + " " + weatherObject.main.humidity + "%";
+      let currentHumid =
+        "Humidity is:" + " " + weatherObject.main.humidity + "%";
       let currentWind = weatherObject.wind.speed + "mph";
-      let currentIcon = `http://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`
+      let currentIcon = `http://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`;
 
       document.getElementById("location").innerHTML = location;
       document.getElementById("currentDate").innerHTML = currentDateEl;
@@ -38,7 +38,7 @@ function getWeather(event, countryCode = "US") {
 
   // Getting the future weather
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?zip=${inputBox.value},${countryCode}&appid=${APIkey}`
+    `https://api.openweathermap.org/data/2.5/forecast?zip=${inputBox.value},${countryCode}&appid=${APIkey}&units=imperial`
   )
     .then((response) => {
       return response.json();
@@ -46,37 +46,39 @@ function getWeather(event, countryCode = "US") {
     .then((weatherObject) => {
       console.log(weatherObject);
 
-    //   let dateTime = new Date(weatherObject.list[2].dt * 1000);
-    //   let date = dateTime.toLocaleDateString("en-US");
-    //   let time = dateTime.toTimeString("it-IT");
-    //   let location = weatherObject.city.name;
-    //   let humidity = "Humidity is:" + " " + weatherObject.list[4].main.humidity + "%";
-    //   let weatherIcon = [weatherObject.list[4].weather.icon];
+        
+      
+      //   let weatherIcon = [weatherObject.list[4].weather.icon];
+      
+      //   document.getElementById("humid").innerHTML = humidity;
+      //   document.getElementById("weatherIcon").innerHTML = weatherIcon;
 
-    
-    //   document.getElementById("humid").innerHTML = humidity;
-    //   document.getElementById("weatherIcon").innerHTML = weatherIcon;
-    
-    let iDontCareForNow = ""
+      let fiveDayCards = "";
 
-    //Loop to dynamically load the 5 day forecast cards
-    for (let index = 4; index <= 36; index+=8) {
+      //Loop to dynamically load the 5 day forecast cards
+      for (let index = 4; index <= 36; index += 8) {
         let currentDate = new Date(weatherObject.list[index].dt * 1000);
-        let currentDateEl = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+        let currentDateEl = `${
+          currentDate.getMonth() + 1
+        }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+        let highTemp = "High Temp:" + " " + weatherObject.list[index].main.temp_max;
+        let lowTemp = "Low Temp:" + " " + weatherObject.list[index].main.temp_min;
+        let humidity = "Humidity:" + " " + weatherObject.list[index].main.humidity + "%";
+        let windSpeed = "Wind Speed:" + " " + weatherObject.list[index].wind.speed;
         
         let currentDiv = `<div class="card" style="width: 18rem;">
         <img src="http://openweathermap.org/img/wn/${weatherObject.list[index].weather[0].icon}.png" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Date: ${currentDateEl}</h5>
+          <h5 class="card-title">${currentDateEl}</h5>
           <ul class="card-text">
-            <li>Humidity</li>
-            <li>Precipitation</li>
-            <li>Wind Speed and direction</li>
+          <li>${highTemp} ${lowTemp}</li>
+            <li>${humidity}</li>
+            <li>${windSpeed}</li>
           </ul>  
         </div>
-      </div>`
-      iDontCareForNow += currentDiv 
-    }
-    document.getElementById("5day").innerHTML = iDontCareForNow;
-});
+      </div>`;
+        fiveDayCards += currentDiv;
+      }
+      document.getElementById("5day").innerHTML = fiveDayCards;
+    });
 }
